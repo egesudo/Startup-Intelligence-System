c4c3ce2544eb1fdf521d30f023bd2765024d426a
EgeKaya <147416148+egesudo@users.noreply.github.com>
2026-08-18 21:51:42 +0300

    Set up Express server with API router

--- api/index.ts
+++ api/index.ts
@@ -1,1 +1,10 @@
+import express from 'express';
+import { apiRouter } from '../src/server/api/routes';
 
+const app = express();
+
+app.use(express.json());
+
+app.use('/', apiRouter);
+
+export default app;
