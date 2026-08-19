7cba795db68d574dcc8f5103b42a66220c55d623
EgeKaya <147416148+egesudo@users.noreply.github.com>
2026-08-19 10:04:49 +0300

    Refactor API index to use server instance

--- api/index.ts
+++ api/index.ts
@@ -1,12 +1,3 @@
-import express from 'express';
-
-const app = express();
-
-app.get('/', (_req, res) => {
-  res.status(200).json({
-    status: 'ok',
-    message: 'Vercel API is working'
-  });
-});
+import app from '../server';
 
 export default app;
